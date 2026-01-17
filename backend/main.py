eab23072447eb219bea5a608bd039a601afc25de
Sid3024 <siddharth.arum@gmail.com>
2026-01-17 19:32:21 +0800

    restructure lang_profile

--- backend/main.py
+++ backend/main.py
@@ -13,12 +13,10 @@ supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
 class UserProfileCreate(BaseModel):
     email : str
-    username : str
 
 class LangProfileCreate(BaseModel):
     lang: str
     level: str
-    purpose: str
 
 class DeckCreate(BaseModel):
     name: str
@@ -26,6 +24,13 @@ class DeckCreate(BaseModel):
 class CardCreate(BaseModel):
     word: str
     translation: str
+
+class UserProfileRetrieve(BaseModel):
+    email: str
+
+@app.get("/user_profiles")
+def retrieve_user_profile(payload: UserProfileRetrieve):
+    res = supabase.table()
 
 @app.post("/user_profiles")
 def create_user(payload: UserProfileCreate):
@@ -43,10 +48,10 @@ def create_lang_profile(user_id: int, payload: LangProfileCreate):
         raise HTTPException(status_code=400, detail=str(res.error))
     return res.data
 
-@app.post("/lang_profile/{lang_profile_id}/decks")
-def create_deck(langProfile_id: int, payload: DeckCreate):
+@app.post("/user_profile/{user_id}/decks")
+def create_deck(user_id: int, payload: DeckCreate):
     row = payload.model_dump()
-    row["lang_profile_id"] = langProfile_id
+    row["user_id"] = user_id
     res = supabase.table("decks").insert(row).execute()
     if getattr(res, "error", None):
         raise HTTPException(status_code=400, detail=str(res.error))
@@ -79,7 +84,6 @@ def create_card(deck_id: int, payload: CardCreate):
         raise HTTPException(status_code=400, detail=card_res.error)
     
     return card_res.data
-
 
 
 
